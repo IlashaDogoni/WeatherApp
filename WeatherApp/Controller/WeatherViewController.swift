7c3650d51028e6c29bbc6064990c3674917db9d7
IlashaDogoni <fistonfire@mail.ru>
2022-01-09 16:38:47 +0600

    End design

---
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Ilya Kokorin on 09.01.2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet var conditionImageView: UIImageView!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
